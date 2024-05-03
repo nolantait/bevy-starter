0106f66ed4370eb569307da47165d9caa3cb71da
Nolan J Tait <nolanjtait@gmail.com>
2024-05-03 10:16:55 -0700

    Updates to bevy 0.13

--- src/physics.rs
+++ src/physics.rs
@@ -1,11 +1,9 @@
 use super::*;
-use bevy_xpbd_2d::prelude::*;
 
 pub struct PhysicsPlugin;
 
 impl Plugin for PhysicsPlugin {
     fn build(&self, app: &mut App) {
         app
-            .add_plugins(PhysicsPlugins::default());
     }
 }
