462bf4ce21e4ce2682c264614c7f897dae2d211c
Nolan J Tait <nolanjtait@gmail.com>
2025-01-18 03:56:36 -0800

    Lifts primary window out of inline definition

--- src/window.rs
+++ src/window.rs
@@ -3,16 +3,18 @@ use bevy::prelude::*;
 const BACKGROUND_COLOR: Color = Color::srgb(0.4, 0.4, 0.4);
 
 pub(super) fn plugin(app: &mut App) {
+    let primary_window = Window {
+        title: "Bevy game".into(),
+        resizable: false,
+        resolution: (800., 600.).into(),
+        canvas: Some("#bevy".to_owned()),
+        desired_maximum_frame_latency: core::num::NonZero::new(1u32),
+        ..default()
+    };
+
     app.insert_resource(ClearColor(BACKGROUND_COLOR))
         .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Bevy game".into(),
-                resizable: false,
-                resolution: (800., 600.).into(),
-                canvas: Some("#bevy".to_owned()),
-                desired_maximum_frame_latency: core::num::NonZero::new(1u32),
-                ..default()
-            }),
+            primary_window: Some(primary_window),
             ..default()
         }));
 }
