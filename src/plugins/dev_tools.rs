202c0aca241cc4449a7ed6fe9e8237089af0fba8
Nolan J Tait <nolanjtait@gmail.com>
2025-07-06 12:18:14 -0700

    Adds pausing plugin

--- src/plugins/dev_tools.rs
+++ src/plugins/dev_tools.rs
@@ -2,15 +2,13 @@
 
 use bevy::{input::common_conditions::input_just_pressed, prelude::*};
 
-pub(crate) fn plugin(app: &mut App) {
-    let toggle_system = toggle_debug_ui.run_if(input_just_pressed(TOGGLE_KEY));
-
-    // Toggle the debug overlay for UI.
-    app.add_systems(Update, toggle_system);
-}
+mod pausing;
 
 const TOGGLE_KEY: KeyCode = KeyCode::Backquote;
 
-fn toggle_debug_ui(mut options: ResMut<UiDebugOptions>) {
-    options.toggle();
+pub(crate) fn plugin(app: &mut App) {
+    app.add_systems(Update, |mut options: ResMut<UiDebugOptions>| {
+        options.toggle().run_if(input_just_pressed(TOGGLE_KEY))
+    })
+    .add_plugins(pausing::PausePlugin);
 }
