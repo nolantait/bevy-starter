87fe5db4b2d2ef014f86d1e5b8b4db8bc863686b
Nolan J Tait <nolanjtait@gmail.com>
2025-09-30 18:49:49 -0700

    Adds better comments for components and resources modules

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,3 +1,5 @@
 //! Components used within this project. Consider importing components using
 //! the `crate::prelude::*` method instead of directly referencing the
 //! `components` module.
+//!
+//! pub(super) mod player;
--- src/resources/mod.rs
+++ src/resources/mod.rs
@@ -1,1 +1,4 @@
-
+//! A list of your resources, one per module
+//! Consider importing resources using the `crate::prelude::*` method instead
+//!
+//! pub(super) mod some_resource;
