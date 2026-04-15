71454779be77d50396f778ffe43365ed55742b7d
Nolan J Tait <nolanjtait@gmail.com>
2026-04-14 21:14:00 -0700

    Adds more diagnostics by default

--- src/plugins/debug.rs
+++ src/plugins/debug.rs
@@ -1,9 +1,14 @@
 use bevy::app::App;
-use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+use bevy::diagnostic::{
+    EntityCountDiagnosticsPlugin, FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin,
+    SystemInformationDiagnosticsPlugin,
+};
 
 pub(crate) fn plugin(app: &mut App) {
     app.add_plugins((
         LogDiagnosticsPlugin::default(),
         FrameTimeDiagnosticsPlugin::default(),
+        EntityCountDiagnosticsPlugin::default(),
+        SystemInformationDiagnosticsPlugin,
     ));
 }
